581abf800fc92d509b45a7417e1d228a45983a73
Mattia Matteini <matteinimattia@gmail.com>
2025-02-20 17:14:47 +0100

    remove R4, add footnotes with links

--- listings/api.py
+++ listings/api.py
@@ -5,28 +5,13 @@ api = Api(questions_bp)
 class QuestionResource(Resource):
 
     def get(self, question_code=None):
-        if question_code:
-            graph_question_id: EntityId = GraphQuestionFactory.id_of(code=question_code)
-            question: Optional[GraphQuestion] = question_service.get_question_by_id(
-                graph_question_id
-            )
-            if question:
-                return serialize(question), StatusCode.OK
-            else:
-                return "Question not found", StatusCode.NOT_FOUND
-        else:
-            all_questions: List = question_service.get_all_questions()
-            all_questions.sort(key=lambda q: q.id)
-            return [serialize(question) for question in all_questions], StatusCode.OK
+        # ...
 
     def post(self):
-        new_question: GraphQuestion = deserialize(request.get_json(), GraphQuestion)
-        try:
-            question_service.add_question(new_question)
-        except ConflictError as e:
-            return e.message, e.status_code
-        return serialize(new_question.id), StatusCode.CREATED
-
-    ...
+        # ...
     
-api.add_resource(QuestionResource, "/questions", "/questions/<string:question_code>")
+api.add_resource(
+    QuestionResource, 
+    "/questions",
+    "/questions/<string:question_code>"
+)
--- listings/kafka.py
+++ listings/kafka.py
@@ -1,7 +1,3 @@
-from typing import List, Callable
-
-from kafka.consumer.fetcher import ConsumerRecord
-
 from application.events import EventsService
 from infrastructure.events import Producer, Consumer
 
@@ -11,12 +7,10 @@ class KafkaEventsService(EventsService):
     def __init__(self):
         self._producer = None
 
-    def publish_message(self, topic: str, message: str) -> None:
+    def publish_message(self, topic: str, message: str):
         if self._producer is None:
             self._producer = Producer()
         self._producer.produce(topic, message)
 
-    def start_consuming(
-        self, topics: List[str], handler: Callable[[ConsumerRecord], None]
-    ) -> None:
+    def start_consuming(self, topics: List[str], handler):
         Consumer(topics, handler).start_consuming()
